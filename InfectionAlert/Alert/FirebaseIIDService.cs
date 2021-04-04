943adce66b3be3068aa4f10f5c4053bc381d715c
Ciucur Andrei <ciucur.andrei@gmail.com>
2021-04-04 21:51:22 +0300

    Add Firebase

---
+++ InfectionAlert/Alert/FirebaseIIDService.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PandemicAlert.Alert
+{
+    [Obsolete]
+    class FirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "FirebaseIIDService";
+
+        [Obsolete]
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendRegistrationToServer(refreshedToken);
+        }
+        void SendRegistrationToServer(string token)
+        {
+            
+        }
+    }
+}
