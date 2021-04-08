b575d1f618898805dc7d6e2324ff7cd36a21a297
Ciucur Andrei <ciucur.andrei@gmail.com>
2021-04-08 22:49:25 +0300

    Add the Settings activity and Preferences class

---
+++ InfectionAlert/Preferences.cs
@@ -0,0 +1,309 @@
+﻿using System.IO;
+
+namespace PandemicAlert.Settings
+{
+
+    public class Preferences
+    {
+
+        private readonly string PATH_SETTINGS = Path.Combine(Xamarin.Essentials.FileSystem.AppDataDirectory, "settings.json");
+        _Preferences preferences;
+
+        public bool ShowInfectionTrend
+        {
+            get
+            {
+                return preferences.ShowInfectionTrend;
+            }
+            set
+            {
+                preferences.ShowInfectionTrend = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowTodayDeaths
+        {
+            get
+            {
+                return preferences.ShowTodayDeaths;
+            }
+            set
+            {
+                preferences.ShowTodayDeaths = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowHealInfectionTrend
+        {
+            get
+            {
+                return preferences.ShowHealInfectionTrend;
+            }
+            set
+            {
+                preferences.ShowHealInfectionTrend = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowActiveCases
+        {
+            get
+            {
+                return preferences.ShowActiveCases;
+            }
+            set
+            {
+                preferences.ShowActiveCases = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowDangerPercentage
+        {
+            get
+            {
+                return preferences.ShowDangerPercentage;
+            }
+            set
+            {
+                preferences.ShowDangerPercentage = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowTodayInfected
+        {
+            get
+            {
+                return preferences.ShowTodayInfected;
+            }
+            set
+            {
+                preferences.ShowTodayInfected = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowTodayHealed
+        {
+            get
+            {
+                return preferences.ShowTodayHealed;
+            }
+            set
+            {
+                preferences.ShowTodayHealed = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowImmunizedPercentage
+        {
+            get
+            {
+                return preferences.ShowImmunizedPercentage;
+            }
+            set
+            {
+                preferences.ShowImmunizedPercentage = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowDeathTrend
+        {
+            get
+            {
+                return preferences.ShowDeathTrend;
+            }
+            set
+            {
+                preferences.ShowDeathTrend = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowIncidence
+        {
+            get
+            {
+                return preferences.ShowIncidence;
+            }
+            set
+            {
+                preferences.ShowIncidence = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowVaccinationRate
+        {
+            get
+            {
+                return preferences.ShowVaccinationRate;
+            }
+            set
+            {
+                preferences.ShowVaccinationRate = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowProjectionImmunization
+        {
+            get
+            {
+                return preferences.ShowProjectionImmunization;
+            }
+            set
+            {
+                preferences.ShowProjectionImmunization = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowProjectionVaccination
+        {
+            get
+            {
+                return preferences.ShowProjectionVaccination;
+            }
+            set
+            {
+                preferences.ShowProjectionVaccination = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowProjectionImmunizationHalf
+        {
+            get
+            {
+                return preferences.ShowProjectionImmunizationHalf;
+            }
+            set
+            {
+                preferences.ShowProjectionImmunizationHalf = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowProjectionVaccinationHalf
+        {
+            get
+            {
+                return preferences.ShowProjectionVaccinationHalf;
+            }
+            set
+            {
+                preferences.ShowProjectionVaccinationHalf = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowProjectionImmunizationQuarter
+        {
+            get
+            {
+                return preferences.ShowProjectionImmunizationQuarter;
+            }
+            set
+            {
+                preferences.ShowProjectionImmunizationQuarter = value;
+                SaveSettings();
+            }
+        }
+        public bool ShowProjectionVaccinationQuarter
+        {
+            get
+            {
+                return preferences.ShowProjectionVaccinationQuarter;
+            }
+            set
+            {
+                preferences.ShowProjectionVaccinationQuarter = value;
+                SaveSettings();
+            }
+        }
+        public bool EnableNotifTodayInfections
+        {
+            get
+            {
+                return preferences.EnableNotifTodayInfections;
+            }
+            set
+            {
+                preferences.EnableNotifTodayInfections = value;
+                SaveSettings();
+            }
+        }
+        public bool EnableNotifTodayHealed
+        {
+            get
+            {
+                return preferences.EnableNotifTodayHealed;
+            }
+            set
+            {
+                preferences.EnableNotifTodayHealed = value;
+                SaveSettings();
+            }
+        }
+        public bool EnableNotifIncidence
+        {
+            get
+            {
+                return preferences.EnableNotifIncidence;
+            }
+            set
+            {
+                preferences.EnableNotifIncidence = value;
+                SaveSettings();
+            }
+        }
+
+        public Preferences()
+        {
+            preferences = new _Preferences();
+
+            LoadSettings();
+        }
+
+        void LoadSettings()
+        {
+            //Load all preferences from file
+            using (var reader = new StreamReader(PATH_SETTINGS, true))
+            {
+                string json = reader.ReadToEnd();
+                
+                preferences = System.Text.Json.JsonSerializer.Deserialize<_Preferences>(json);
+            }
+        }
+
+        async void SaveSettings()
+        {
+            string jsonStr = System.Text.Json.JsonSerializer.Serialize(preferences);
+
+            using (var writer = File.CreateText(PATH_SETTINGS))
+            {
+                await writer.WriteAsync(jsonStr);
+                writer.Close();
+            }
+        }
+
+        private class _Preferences
+        {
+            public bool ShowInfectionTrend;
+            public bool ShowHealInfectionTrend;
+            public bool ShowActiveCases;
+            public bool ShowDangerPercentage;
+            public bool ShowTodayInfected;
+            public bool ShowTodayHealed;
+            public bool ShowImmunizedPercentage;
+            public bool ShowTodayDeaths;
+            public bool ShowDeathTrend;
+            public bool ShowIncidence;
+            public bool ShowVaccinationRate;
+            public bool ShowProjectionImmunization;
+            public bool ShowProjectionVaccination;
+            public bool ShowProjectionImmunizationHalf;
+            public bool ShowProjectionVaccinationHalf;
+            public bool ShowProjectionImmunizationQuarter;
+            public bool ShowProjectionVaccinationQuarter;
+
+            public bool EnableNotifTodayInfections;
+            public bool EnableNotifTodayHealed;
+            public bool EnableNotifIncidence;
+
+        }
+
+    }
+}
---
+++ InfectionAlert/SettingsActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Text.Style;
+using Android.Util;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.Core.App;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Android.Content.PM;
+using System.Threading.Tasks;
+
+namespace PandemicAlert
+{
+    [Activity(Label = "@string/settings_activity_title", Theme = "@style/AppTheme", ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SettingsActivity : AppCompatActivity
+    {
+
+        Switch switch_showInfectionTrend, switch_showHealingInfectionTrend, switch_showActiveCases, switch_deathPercentage;
+        Switch switch_showTodayInfections, switch_showTodayHealed, switch_showTodayDeaths;
+        Switch switch_showDeathTrend;
+        Switch switch_showIncidence;
+        Switch switch_showVaccinationRate, switch_showImmunizedPercentage;
+        Switch switch_showProjectedImmunization, switch_showProjectedVaccination, switch_showProjectedImmunizationHalf, switch_showProjectedVaccinationHalf, switch_showProjectedImmunizationQuarter, switch_showProjectedVaccinationQuarter;
+        Switch switch_showVaccinatedPercentage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_settings);
+
+            switch_showInfectionTrend = FindViewById<Switch>(Resource.Id.switch_show_infection_trend);
+            switch_showHealingInfectionTrend = FindViewById<Switch>(Resource.Id.switch_show_heal_infection_trend);
+            switch_showActiveCases = FindViewById<Switch>(Resource.Id.switch_show_active_cases);
+            switch_deathPercentage = FindViewById<Switch>(Resource.Id.switch_show_death_percentage);
+            switch_showTodayInfections = FindViewById<Switch>(Resource.Id.switch_show_today_infections);
+            switch_showTodayHealed = FindViewById<Switch>(Resource.Id.switch_show_today_healed);
+            switch_showTodayDeaths = FindViewById<Switch>(Resource.Id.switch_show_today_deaths);
+            switch_showDeathTrend = FindViewById<Switch>(Resource.Id.switch_show_death_trend);
+            switch_showIncidence = FindViewById<Switch>(Resource.Id.switch_show_incidence);
+            switch_showVaccinationRate = FindViewById<Switch>(Resource.Id.switch_show_vaccination_rate);
+            switch_showImmunizedPercentage = FindViewById<Switch>(Resource.Id.switch_show_immunized_percentage);
+            switch_showProjectedImmunization = FindViewById<Switch>(Resource.Id.switch_show_proj_immunization);
+            switch_showProjectedVaccination = FindViewById<Switch>(Resource.Id.switch_show_proj_vaccination);
+            switch_showVaccinatedPercentage = FindViewById<Switch>(Resource.Id.switch_show_vaccinated_percentage);
+
+            switch_showProjectedImmunizationHalf = FindViewById<Switch>(Resource.Id.switch_show_proj_immunization_half);
+            switch_showProjectedVaccinationHalf = FindViewById<Switch>(Resource.Id.switch_show_proj_vaccination_half);
+            switch_showProjectedImmunizationQuarter = FindViewById<Switch>(Resource.Id.switch_show_proj_immunization_quarter);
+            switch_showProjectedVaccinationQuarter = FindViewById<Switch>(Resource.Id.switch_show_proj_vaccination_quarter);
+
+
+        }
+    }
+}
